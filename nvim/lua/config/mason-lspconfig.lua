2a6795e75a5dfb0af216f30d2ce61053807b8d8c
Michiel Van Couwenberghe <michielvancouwenberghe@gmail.com>
2022-12-19 20:03:31 +0100

    automatic installation

--- nvim/lua/config/mason-lspconfig.lua
+++ nvim/lua/config/mason-lspconfig.lua
@@ -1,7 +1,10 @@
-require("mason-lspconfig").setup()
+require("mason-lspconfig").setup({
+    automatic_installation = true
+})
 
 local lspconfig = require("lspconfig")
 
+-- lsp servers must be setup after setting up mason
 lspconfig.bashls.setup({})
 lspconfig.jsonls.setup({})
 lspconfig.vimls.setup({})
