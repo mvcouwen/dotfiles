01ea2b852ba1fa3485e729ea799d446874fd58e0
Michiel Van Couwenberghe <michielvancouwenberghe@gmail.com>
2022-12-20 23:31:16 +0100

    cleanup

--- nvim/lua/config/nvim-autopairs.lua
+++ nvim/lua/config/nvim-autopairs.lua
@@ -2,7 +2,7 @@ require("nvim-autopairs").setup()
 
 -- automatically insert brackets after completing a function or method
 local cmp = require("cmp")
-local cmp_autopairs = require("nvim-autopairs/completion/cmp")
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 cmp.event:on(
     "confirm_done",
     cmp_autopairs.on_confirm_done({
